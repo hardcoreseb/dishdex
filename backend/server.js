811eb56ee36f3f77c3b43222295e2d5defbcbe69
hardcoreseb <sebastian.reinhold@yahoo.de>
2024-02-10 02:35:52 +0100

    Add backend environment

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+// backend/server.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000; // Choose any port you like
+const path = require('path');
+
+app.get("/", function (req, res) {
+    res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+app.use(express.static(path.join(__dirname, '../frontend/dist/dishdex')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/dishdex/browser/index.html'));
+});
