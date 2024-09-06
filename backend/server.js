9fd33f2a0dd14042c74044ab4aa70fc3be175c8b
hardcoreseb <sebastian.reinhold@yahoo.de>
2024-09-06 19:58:47 +0200

    Communication fixes

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,20 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000; // Choose any port you like
 const path = require('path');
+const favicon = require('serve-favicon');
 
-app.get("/", function (req, res) {
-    res.send("Hello World!");
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
-});
+// Serve the favicon
+app.use(favicon(path.join(__dirname, '../frontend/dist/dishdex/browser/favicon.ico')));
 
 app.use(express.static(path.join(__dirname, '../frontend/dist/dishdex')));
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+app.get("/test", function (req, res) {
+    res.send("Hello World!");
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/dishdex/browser/index.html'));
